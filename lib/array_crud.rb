0ae7b9b5b730215d571cda077bc2314f9637585b
Matthew Malecki <mmalecki4@gmail.com>
2020-04-14 22:56:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,21 +9,21 @@ end
 def add_element_to_end_of_array(array, element)
   array = array.push(element)  
 end
-
+ 
 def add_element_to_start_of_array(array, element)
-  
+   array = array.unshift(element)   
 end
 
 def remove_element_from_end_of_array(array)
-  
+   array = array.pop(element)   
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array = array.shift(element)    
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+   array = array.push(element)  
 end
 
 def retrieve_first_element_from_array(array)
